dba42a0ae3793956746bdc45fa6dfb94682c0746
L337hium <eddy@sdf.org>
2014-04-04 20:28:29 +0200

    neigh highlight

--- d3topo.js
+++ d3topo.js
@@ -2,15 +2,9 @@ d3.tsv("current-topo.tsv", function(d) {
 
 var json = (function () {
     var json = null;
-    $.ajax({
-        'async': false,
-        'global': false,
-        'url': "http://localhost:8000/current-hostnames.json",
-        'dataType': "json",
-        'success': function (data) {
-            json = data;
-        }
-    });
+    $.ajax({ 'async': false, 'global': false,
+        'url': "current-hostnames.json", 'dataType': "json",
+        'success': function (data) { json = data; } });
     return json;
 })(); 
 
@@ -20,8 +14,8 @@ console.log(json);
 	height = 1680;
 
 	var force = d3.layout.force()
-			.charge(-120) /*-120*/
-			.linkDistance(Math.random()*50*1/cost(d.cost)) /* 75 25 */
+			.charge(-125) /*-120*/
+			.linkDistance(function(d){ return 700*cost(d) }) /* 75 25 */
 			.size([width, height]);
 
 	var svg = d3.select("#graph")
@@ -59,16 +53,13 @@ console.log(json);
 				"source": ip_lookup[d.dest],
 				"target": ip_lookup[d.src],
 				"value": cost(d.cost)
-			})
-		})
-	}
+			}) }) }
 
 	graph.links.forEach(function(link) {
 		linkNodes.push({
 			source: graph.nodes[link.source],
 			target: graph.nodes[link.target]
-		});
-	});
+		}); });
 
 	force
 		.nodes(graph.nodes.concat(linkNodes))
@@ -80,43 +71,9 @@ console.log(json);
 			.enter()
 			.append("line")
 			.attr("class", "link")
-			.style("stroke-width", function(d) { return Math.sqrt(d.value); });
-
-
-	var node = svg.selectAll(".node")
-			.data(graph.nodes)
-			.enter()
-			.append("g")
-			.call(force.drag);
-
-		node.append("circle")
-			.attr("class", "node")
-			.attr("r", function(d) { return (radius(d.degree)) })
-			.attr("name", function(d) { return (d.name) });
-
-		/*node.on('mouseover', function(d) {
-			d3.select(this).style({ fill: 'red' });
-		});
-		node.on('mouseout', function(d) {
-			d3.select(this).style({ fill: '#ccc' });
-		});*/
-
-		/*node.append("title")
-			.text(function(d) { return d.name; });*/
-
-		node.append("svg:a")
-			.attr("dx", 24)
-			.attr("dy", 24 /*".35em"*/)
-			.style("font-size", "10px")
-			.attr("xlink:href", function(d) { return "http://"+d.name; })
-			.append("svg:text").text(function(d) { var name = d.name; return d.name+" - "+json[name]; });
-
-/*		node.append("text")
-			.attr("dx", 12)
-			.attr("dy", ".35em")
-			.style("font-size", "10px")
-			.text(function(d) { return d.name });
-*/		 
+			.style("stroke-width", function(d) { return Math.sqrt(d.value); })
+			.on("mouseover", linkMouseover)
+			.on("mouseout", mouseout);
 
 	var linkNode = svg.selectAll(".link-node")
 			.data(linkNodes)
@@ -126,6 +83,44 @@ console.log(json);
 			.attr("r", 2)
 			.style("fill", "#ccc");
 
+	var node = svg.selectAll(".node")
+			.data(graph.nodes)
+			.enter()
+			.append("g")
+			.call(force.drag);
+
+		node.append("circle")
+				.attr("class", "node")
+				.attr("r", function(d) { return (radius(d.degree)) })
+				.attr("name", function(d) { return (d.name) })
+				.on("mouseover", nodeMouseover)
+				.on("mouseout", mouseout);
+
+		node.append("svg:a")
+			.attr("dx", 24)
+			.attr("dy", ".35em")
+			.style("font-size", "11px")
+			.attr("xlink:href", function(d) { return "http://"+d.name; })
+			.append("svg:text").text(function(d) { var name = d.name; return d.name+" - "+json[name]; });
+
+function linkMouseover(d) {
+	svg.selectAll(".node").classed("active", function(p) { return d3.select(this).classed("active") || p === d.source || p === d.target; });
+	svg.selectAll(".node circle").classed("active", function(p) { return p === d.source || p === d.target; });
+}
+
+function nodeMouseover(d) {
+	svg.selectAll(".node").classed("active", function(p) { return p.source === d || p.target === d; });
+
+	svg.selectAll(".link").classed("active", function(p) { return d3.select(this).classed("active") || p.source === d || p.target === d; });
+	svg.selectAll(".link.active").each(function(d){linkMouseover(d)})
+
+	d3.select(this).classed("active", true);
+}
+
+function mouseout() {
+	svg.selectAll(".active").classed("active", false);
+}
+
 	force.on("tick", function() {
 		link.attr("x1", function(d) { return d.source.x; })
 			.attr("y1", function(d) { return d.source.y; })
@@ -134,7 +129,7 @@ console.log(json);
 		node.attr("cx", function(d) { return d.x; })
 			.attr("cy", function(d) { return d.y; })
 			.attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; });
-		linkNode.attr("cx", function(d) { return d.x = (d.source.x + d.target.x) * 0.5; })
-			.attr("cy", function(d) { return d.y = (d.source.y + d.target.y) * 0.5; });
+//		linkNode.attr("cx", function(d) { return d.x = (d.source.x + d.target.x) * 0.5; })
+//			.attr("cy", function(d) { return d.y = (d.source.y + d.target.y) * 0.5; });
 	});
 });
